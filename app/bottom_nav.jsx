6a2eb42c27996e5180a72a00062583e487c6f4e7
Zaenal <zaenal@getsurvey.id>
2025-03-17 09:54:03 +0800

    home screen

--- app/bottom_nav.jsx
+++ app/bottom_nav.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { BottomNavigation, BottomNavigationTab, Icon } from '@ui-kitten/components';
-import { useState } from 'react';
+import React, { useState } from "react";
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Icon,
+} from "@ui-kitten/components";
+
+const HomeIcon = (props) => <Icon {...props} name="home-outline" />;
+const HistoryIcon = (props) => <Icon {...props} name="list-outline" />;
+const CustomerIcon = (props) => <Icon {...props} name="people-outline" />;
+const ProfileIcon = (props) => <Icon {...props} name="person-outline" />;
 
 const BottomNav = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
-
-  const HomeIcon = (props) => <Icon {...props} name="home-outline" />;
-  const HistoryIcon = (props) => <Icon {...props} name="list-outline" />;
-  const CustomerIcon = (props) => <Icon {...props} name="people-outline" />;
-  const ProfileIcon = (props) => <Icon {...props} name="person-outline" />;
 
   return (
     <BottomNavigation
